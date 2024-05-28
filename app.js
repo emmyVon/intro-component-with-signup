de40e255a906ad1cbab87206b2ff0a01db3841d6
EMMANUEL OBIEZUO <emmanuelobiezuo@gmail.com>
2024-05-29 03:52:31 +0800

    active states

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const lastName = document.querySelector('.lastName');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 
-console.log(firstName);
 
 // console.log(firstName, lastName, email, password);
 
@@ -15,35 +14,66 @@ form.addEventListener('submit', (e) => {
   const lName = lastName.value;
   const emailVal = email.value;
   const passwordVal = password.value;
-  console.log(fName, lName, emailVal, passwordVal);
+ 
+  const previouserror = document.querySelectorAll('.error-text')
 
+  previouserror.forEach(data=>{
+    if(data){
+      data.remove()
+    }
+  })
   // Check first name
   if (fName === '') {
     firstName.classList.add('error');
+     const error = document.createElement('p');
+     error.classList.add('error-text')
+      error.textContent = 'FirstName cannot be empty'
+      firstName.parentElement.appendChild(error)
+      firstName.parentElement.classList.add('error-circle')
+    
   } else {
     firstName.classList.remove('error');
+    firstName.parentElement.classList.remove('error-circle')
   }
   // Check last name
 
   if (lName === '') {
     lastName.classList.add('error');
+    const error = document.createElement('p');
+    error.classList.add('error-text')
+    error.textContent = 'LastName cannot be empty'
+      lastName.parentElement.appendChild(error)
+      lastName.parentElement.classList.add('error-circle')
   } else {
     lastName.classList.remove('error');
+    lastName.parentElement.classList.remove('error-circle')
   }
   // Check email
 
   if (!validateEmail(emailVal) || emailVal === '') {
     email.classList.add('error');
+    const error = document.createElement('p');
+    error.classList.add('error-text')
+    error.textContent = 'email cannot be empty'
+    email.parentElement.appendChild(error)
+    email.parentElement.classList.add('error-circle')
   } else {
     email.classList.remove('error');
+    email.parentElement.classList.remove('error-circle')
   }
 
   // Check password
 
   if (passwordVal === '') {
     password.classList.add('error');
+    const error = document.createElement('p');
+    error.classList.add('error-text')
+     error.textContent = 'password cannot be empty'
+    password.parentElement.appendChild(error)
+    password.parentElement.classList.add('error-circle')
   } else {
     password.classList.remove('error');
+    password.parentElement.classList.remove('error-circle')
   }
 });
 
@@ -53,3 +83,4 @@ function validateEmail(email) {
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
